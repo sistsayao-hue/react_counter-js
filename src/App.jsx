b125c181fdc2453b658abed0bd8078406f417fc6
Rodrigo <sistsayao@gmail.com>
2026-07-11 14:26:00 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import './App.scss';
 export const App = () => {
   const [count, setCount] = useState(0);
 
-  const addOne = () => {
-    setCount(prevCount => prevCount + 1);
-  };
+const addOne = () => {
+  setCount(prevCount => {
+    const newCount = prevCount + 1;
+    return newCount % 5 === 0 ? newCount + 100 : newCount;
+  });
+};
+
 
   const add100 = () => {
     setCount(prevCount => prevCount + 100);

98e946059d80b3f82c8d6a6e8be848fb00f7b53c
Rodrigo <sistsayao@gmail.com>
2026-07-13 19:05:53 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,55 @@
-export const App = () => { const [count, setCount] = useState(0);
+import { useState } from 'react';
 
-const addOne = () => { setCount((prev) => prev + 1); };
+export const App = () => {
+  const [count, setCount] = useState(0);
 
-const add100 = () => { setCount((prev) => prev + 100); };
+  const addOne = () => {
+    setCount(prev => prev + 1);
+  };
 
-// DON'T change the code below const increase = () => { if (count % 5 === 0) { add100(); }
+  const add100 = () => {
+    setCount(prev => prev + 100);
+  };
 
-Code
-addOne();
+  // DON'T change the code below
+  const increase = () => {
+    if (count % 5 === 0) {
+      add100();
+    }
+
+    addOne();
+  };
+
+  return (
+    <div className="App">
+      <h1 className="App__title">
+        Count: {count}
+      </h1>
+
+      <button
+        type="button"
+        className="App__add-one"
+        onClick={addOne}
+      >
+        Add 1
+      </button>
+
+      <button
+        type="button"
+        className="App__add-100"
+        onClick={add100}
+      >
+        Add 100
+      </button>
+
+      <button
+        type="button"
+        className="App__increase"
+        onClick={increase}
+      >
+        Increase
+      </button>
+    </div>
+  );
 };
 
-return ( <div className="App"> <h1 className="App__title">{Count: ${count}}</h1>
-
-Code
-  <button type="button" className="App__add-one" onClick={addOne}>
-    Add 1
-  </button>
-
-  <button type="button" className="App__add-100" onClick={add100}>
-    Add 100
-  </button>
-
-  <button type="button" className="App__increase" onClick={increase}>
-    Increase
-  </button>
-</div>
-); };

39a105c1c49ad617db55b526694ba5afb5ef4c7d
Rodrigo <sistsayao@gmail.com>
2026-07-13 18:59:21 -0300

    Refactor App component to simplify state updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,28 @@
-import { useState } from 'react';
-import './App.scss';
+export const App = () => { const [count, setCount] = useState(0);
 
-export const App = () => {
-  const [count, setCount] = useState(0);
-const addOne = () => {
-  setCount(prevCount => {
-    const newCount = prevCount + 1;
-    return newCount % 5 === 0 ? newCount + 100 : newCount;
-  });
+const addOne = () => { setCount((prev) => prev + 1); };
+
+const add100 = () => { setCount((prev) => prev + 100); };
+
+// DON'T change the code below const increase = () => { if (count % 5 === 0) { add100(); }
+
+Code
+addOne();
 };
 
-const add100 = () => {
-  setCount(prevCount => {
-    const newCount = prevCount + 100;
+return ( <div className="App"> <h1 className="App__title">{Count: ${count}}</h1>
 
-    return newCount % 5 === 0 ? newCount + 100 : newCount;
-  });
-};
+Code
+  <button type="button" className="App__add-one" onClick={addOne}>
+    Add 1
+  </button>
 
+  <button type="button" className="App__add-100" onClick={add100}>
+    Add 100
+  </button>
 
-
-  // DON'T change the code below
-  const increase = () => {
-    if (count % 5 === 0) {
-      add100();
-    }
-
-    addOne();
-  };
-
-  return (
-    <div className="App">
-      <h1 className="App__title">{`Count: ${count}`}</h1>
-
-      <button type="button" className="App__add-one" onClick={addOne}>
-        Add 1
-      </button>
-
-      <button type="button" className="App__add-100" onClick={add100}>
-        Add 100
-      </button>
-
-      <button type="button" className="App__increase" onClick={increase}>
-        Increase
-      </button>
-    </div>
-  );
-};
+  <button type="button" className="App__increase" onClick={increase}>
+    Increase
+  </button>
+</div>
+); };
